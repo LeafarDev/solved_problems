8b1d121df90397cb4774ea4fc67ee135400f3650
ew <rafael.o@aluno.ifsp.edu.br>
2020-07-09 20:39:13 -0300

    Initial

---
+++ test/solved_problems/core_test.clj
@@ -0,0 +1,7 @@
+(ns solved-problems.core-test
+  (:require [clojure.test :refer :all]
+            [solved-problems.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
