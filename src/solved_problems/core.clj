d327e6fd093169450391c2e42df7ed3e023ce5ab
ew <rafael.o@aluno.ifsp.edu.br>
2020-07-15 14:47:09 -0300

    problems 6, 7, 8

--- src/solved_problems/core.clj
+++ src/solved_problems/core.clj
@@ -131,4 +131,47 @@
                              (key %)) range-frequencies)))) set-keys)))))
 (problem-five)
 ;; (problem-five-brute-force)
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn problem-six
+  "The sum of the squares of the first ten natural numbers is 1^2 + 2^2 +...+ 10^2 =385
+  The square of the sum of the first ten natural numbers is (1+2+...+10)^2=552=3025
+  Hence the difference between the sum of the squares of the first ten natural numbers and
+  the square of the sum is 3025−385=2640
+  Find the difference between the sum of the squares of the first one hundred natural numbers
+  and the square of the sum."
+  []
+  (bigint (- (Math/pow (reduce + (range 1 101)) 2)
+             (reduce #(+ %1 (Math/pow %2 2)) (range 1 101)))))
+(problem-six)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn problem-seven []
+  "By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+  the 6th prime is 13.
+  What is the 10 001st prime number?"
+  (last (loop [primes [2]]
+          (if (= (count primes) 10001)
+            primes
+            (recur (conj primes (next-prime (last primes))))))))
+(problem-seven)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(defn problem-eight
+  "The four adjacent digits in the 1000-digit number that have
+the greatest product are 9 × 9 × 8 × 9 = 5832.
+Find the thirteen adjacent digits in the 1000-digit
+number that have the greatest product. What is the value of this product?"
+  []
+  (let [very-big-number (bigint 7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450)
+        digits (map #(bigint (str %)) (take 1000 (str very-big-number)))]
+    (loop [index 0 largest-product {:digits [] :product 0}]
+      (if (<= (+ index 13) (count digits))
+        (let [last-largest-product (get largest-product :product)
+              current-digits (subvec (vec digits) index (+ index 13))
+              product-current-range (reduce * current-digits)]
+          (if (> product-current-range last-largest-product)
+            (recur (inc index) {:digits current-digits :product (bigint product-current-range)})
+            (recur (inc index) largest-product)))
+        largest-product))))
+(problem-eight)
